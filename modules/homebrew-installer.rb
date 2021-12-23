2bdeccbcdb9974ab6314f9f5e10c140512016562
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2021-12-23 15:11:02 +0000

    Simplified download of script file

--- modules/homebrew-installer.rb
+++ modules/homebrew-installer.rb
@@ -2,9 +2,9 @@
 
 #
 # os-checker.rb file
-# Created by Liam Powell (gfelipe099)
+# Created by Ken Hoo (mrkenhoo)
 # for macOS amd64
 #
 def installHomebrew
-    system ("if [ ! -f ./install.sh ]; then curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > install.sh; else echo 'File install.sh already exists'; fi; sh install.sh && rm install.sh")
+    system ("[ ! -f install.sh ] && curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > install.sh && sh install.sh && rm install.sh")
 end
